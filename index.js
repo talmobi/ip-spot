ca1e439c2f166e6c0cffee4a5c6330b47b9e3149
Christian Talmo <talmo.christian@gmail.com>
2016-03-13 13:10:22 +0200

    dns-lookup functionality

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 var http = require('http');
-var app = require('express')();
+var express = require('express');
+var app = express();
 
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var extend = require('extend');
+
+var dns = require('dns');
+var URL = require('url');
+
+var request = require('request');
 
 var jade = require('jade');
 var locals = {
   title: 'ip-spot.com - your public ip',
 };
 
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+
 var url = "mongodb://localhost:27017/ip-spot";
 MongoClient.connect(url, function (err, db) {
-	assert.equal(null, err);
-	console.log("Connected to mongodb server");
-
-  var collection = db.collection('ips');
+  assert.equal(null, err);
+  console.log("Connected to mongodb server");
 
   app.get('/', function (req, res) {
+    var collection = db.collection('ips');
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-	ip = ip.split(/,*\s+/)[0];
+    ip = ip.split(/,*\s+/)[0];
 
-	console.log(ip);
+    console.log(ip);
 
     var doc = {
       ip: ip,
@@ -36,14 +44,19 @@ MongoClient.connect(url, function (err, db) {
       console.log("saved ip [%s] to mongodb", ip);
     });
 
-    collection.find().sort({_id: -1}).limit(10).toArray(function (err, docs) {
+    db.collection('dns-lookups').find().sort({_id: -1}).limit(10).toArray(function (err, docs) {
       if (err) {
+        res.status(500).json(err).end();
         return console.error(err);
       }
 
+      var filter = docs.filter(function (val, ind, arr) {
+        return val && val.addresses && val.addresses[0];
+      });
+
       var data = {
         ip: ip,
-        recent_ips: docs
+        recent_lookups: filter
       };
 
       var html = jade.renderFile('./jade/index.jade', extend(locals, data));
@@ -52,8 +65,41 @@ MongoClient.connect(url, function (err, db) {
     });
 
   });
-});
 
+  app.get('/:url', function (req, res) {
+    var collection = db.collection('dns-lookups');
+
+    console.log("url: %s", req.params.url);
+    var url = req.params.url;
+    if (url.indexOf('http') == -1) {
+      url = 'http://' + url;
+    }
+
+    var hostname = URL.parse( url ).hostname;
+    console.log("hostname: %s", hostname);
+
+    dns.lookup(hostname, { all: true }, function (err, addresses) {
+      if (err) {
+        res.status(500).json(err).end();
+        return console.error(err);
+      };
+
+      var doc = {
+        hostname: hostname,
+        addresses: addresses
+      };
+
+      res.json(doc).end();
+
+      collection.insert(doc, function (err, result) {
+        if (err) {
+          return console.error(err);
+        }
+        console.log("saved dns-lookup [%s] to mongodb", doc.hostname);
+      });
+    });
+  });
+});
 
 var port = 3060;
 
---
+++ js/app.js
@@ -0,0 +1,38 @@
+console.log("app js loaded");
+
+var inputEl = document.getElementById('search');
+var submitEl = document.getElementById('form');
+
+function handleError () {
+  //inputEl.value = "";
+};
+
+submitEl.onsubmit = function (e) {
+  e.preventDefault();
+  console.log("looking up: " + inputEl.value);
+
+  var val = inputEl.value;
+  var hostname = val.substring();
+  var ind = hostname.indexOf('/');
+  if (ind > 0) {
+    hostname = val.substring(0, ind);
+  }
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/' + hostname, true);
+  xhr.onload = function () {
+    if (xhr.status >= 200 && xhr.status < 400) {
+      // success
+      window.location.reload();
+    } else {
+      // server reached, but error
+      handleError();
+    }
+  };
+
+  xhr.onerror = handleError;
+
+  xhr.send();
+};
+
+inputEl.focus();
