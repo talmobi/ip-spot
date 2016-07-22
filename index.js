b01ef2c7c5dc18d5f1117f8e91bf12baf7af2fca
Christian Talmo <talmo.christian@gmail.com>
2016-07-22 17:31:33 +0300

    simplified app a lot, only return ip

--- index.js
+++ index.js
@@ -2,178 +2,29 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
-var r = require('rethinkdb');
+var RateLimit = require('express-rate-limit')
+var ipaddr = require('ipaddr.js') // manipulate ipv4 and ipv6 addresses
 
-var assert = require('assert');
-var extend = require('extend');
+var limiter = RateLimit({
+  windowMs: 1000 * 60 * 5, // 5 min
+  max: 100,
+  delayMs: 0
+})
 
-var dns = require('dns');
-var URL = require('url');
+app.use(limiter)
 
-// used to parse for ip lookup to iplocation.net
-var request = require('request');
-var cheerio = require('cheerio');
+app.use(function (req, res) {
+  var ip = req.headers['x-forwarded-for'] || req.ip || req.connection.remoteAddress
+  var ipaddr_ = ipaddr.parse(ip)
 
-var jade = require('jade');
-var locals = {
-  title: 'ip-spot.com - your public ip',
-};
+  if (ipaddr_.isIPv4MappedAddress()) {
+    ipaddr_ = ipaddr_.toIPv4Address()
+  }
 
-function iplocationLookup (hostname, done) {
-  var url = "https://www.iplocation.net";
-  var formData = {
-    query: hostname,
-    submit: "IP Lookup"
-  };
-
-  request({
-    method: 'POST',
-    url: url,
-    formData: formData
-  }, function (error, response, body) {
-    if (error) {
-      return done(error);
-    }
-
-    var $ = cheerio.load(body);
-    var tds = $("tbody tr td");
-
-    var spans = $('span');
-    var text = spans.text();
-
-    var results = /(\d+\.{1}\d+\.{1}\d+\.{1}\d+)/.exec( text );
-    var ip = results[0] || results[1];
-
-    var data = {
-      ip: ip,
-      hostname: $(tds[0]).text(),
-      country: $(tds[1]).text(),
-      region: $(tds[2]).text(),
-      city: $(tds[3]).text()
-    };
-
-    return done(null, data);
-  });
-};
-
-function dnsLookup (hostname, done) {
-  dns.lookup(hostname, { all: true }, function (err, addresses) {
-    if (err) {
-      return done(err);
-    };
-
-    var data = {
-      ip: addresses[0].address,
-      addresses: addresses
-    };
-
-    return done(null, data);
-  });
-};
-
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/js', express.static(__dirname + '/js'));
-app.use('/assets', express.static(__dirname + '/assets'));
-
-var connection = null;
-r.connect({host: 'localhost', port: 28015 }, function (err, conn) {
-  if (err) throw err;
-  connection = conn;
-  console.log("connected to rethinkdb");
-
-  // create table, ignore error if it already exists
-  r.tableCreate('ip_spot').run(connection, function (err, result) {
-    if (err) {
-      // ignore, probably because the table already exists...
-    }
-  });
-
-  app.get('/', function (req, res) {
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-    ip = ip.split(/,*\s+/)[0];
-
-    console.log(ip);
-
-    var doc = {
-      type: 'pageview',
-      created_at: Date.now(),
-      ip: ip,
-      remoteAddress: req.connection.remoteAddress,
-      'x-forwarded-for': req.headers['x-forwarded-for']
-    };
-
-    r.table('ip_spot').insert( doc ).run(connection, function (err, result) {
-      if (err) throw err;
-      console.log("saved ip [%s] to rethinkdb", ip);
-    })
-
-    r.table('ip_spot').filter({type: 'search'}).orderBy(r.desc('created_at')).limit(10).run(connection, function (err, cursor) {
-      if (err) throw err;
-      cursor.toArray(function (err, docs) {
-        if (err) throw err;
-
-        var data = {
-          ip: ip, // your ip
-          recent_lookups: docs
-        };
-        var html = jade.renderFile('./jade/index.jade', extend(locals, data));
-        return res.send(html).end();
-      });
-    });
-  });
-
-  app.get('/api/:url', function (req, res) {
-
-    console.log("url: %s", req.params.url);
-    var url = req.params.url;
-    if (url.indexOf('http') == -1) {
-      url = 'http://' + url;
-    }
-
-    var hostname = URL.parse( url ).hostname;
-    console.log("hostname: %s", hostname);
-
-    iplocationLookup (hostname, function (err, data) {
-      if (err) {
-        res.status(500).json(err).end();
-        return console.error(err);
-      };
-
-      var doc = Object.assign({}, data);
-      doc.type = "search";
-      doc.created_at = Date.now();
-
-      var finish = function (doc) {
-        res.json(doc).end();
-
-        r.table('ip_spot').insert(doc).run(connection, function (err, result) {
-          if (err) throw err;
-          console.log("saved ip-data [%s] to rethinkdb", doc.hostname);
-        });
-      };
-
-      doc.ip = null;
-      if (!doc.ip || typeof doc.ip !== 'string' || doc.ip.length < 5) {
-        console.log("using dns lookup as backup");
-        // grab ip from dns lookup
-        dnsLookup(hostname, function (err, data) {
-          if (err) {
-            res.status(500).json(err).end();
-            return console.error(err);
-          }
-          Object.assign(doc, data);
-          console.log(doc);
-          finish(doc);
-        });
-      } else {
-        console.log("used only iplocationLookup");
-        finish(doc);
-      }
-    });
-  });
-
-});
+  var ipstr = ipaddr_.toString()
+  console.log('[%s] hit by [%s]', ipaddr_.kind(), ipstr)
+  res.send( ipstr )
+})
 
 var port = 3060;
 
